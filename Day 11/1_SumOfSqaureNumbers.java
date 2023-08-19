104767d15def97276e7ca5e2fa4cdf268eb1a063
developer-priya-mishra <mishrapriya549@gmail.com>
2023-08-19 22:32:48 +0530

    Day 11

---
+++ Day 11/1_SumOfSqaureNumbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class SumOfSqaureNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int num = scan.nextInt();
+
+        int sum = 0;
+        for (int i = 1; i <= num; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println("Sum of " + num + " even natural numbers is " + sum);
+
+        scan.close();
+    }
+}
