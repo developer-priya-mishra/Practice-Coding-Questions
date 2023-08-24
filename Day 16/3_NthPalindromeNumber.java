7deae37a33192580170435022c2963e2567b1535
developer-priya-mishra <mishrapriya549@gmail.com>
2023-08-24 10:53:47 +0530

    Day 16

--- Day 16/3_NthPalindromeNumber.java
+++ Day 16/3_NthPalindromeNumber.java
@@ -1,16 +1,13 @@
 import java.util.Scanner;
 
-class PrintPalindromeNumber {
+class NthPalindromeNumber {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Enter the first number : ");
-        int num1 = scan.nextInt();
+        System.out.print("Enter the nth palindrome no : ");
+        int num = scan.nextInt();
 
-        System.out.print("Enter the second number : ");
-        int num2 = scan.nextInt();
-
-        for (int i = num1; i <= num2; i++) {
+        for (int i = 1; i <= num; i++) {
             int tempNum = i;
             int remainder = 0;
             int reverse = 0;
@@ -19,7 +16,7 @@ class PrintPalindromeNumber {
                 reverse = reverse * 10 + remainder;
                 tempNum = tempNum / 10;
             }
-            // System.out.println(reverse);
+
             if (reverse == i) {
                 System.out.println(reverse);
             }
