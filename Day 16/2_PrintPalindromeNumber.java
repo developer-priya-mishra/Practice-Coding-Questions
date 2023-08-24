b48e06229247da197796de6377d2b904f6dddeda
developer-priya-mishra <mishrapriya549@gmail.com>
2023-08-24 10:43:52 +0530

    Day 16

---
+++ Day 16/2_PrintPalindromeNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class PrintPalindromeNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int num = scan.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            int tempNum = i;
+            int remainder = 0;
+            int reverse = 0;
+            while (tempNum != 0) {
+                remainder = tempNum % 10;
+                reverse = reverse * 10 + remainder;
+                tempNum = tempNum / 10;
+            }
+            // System.out.println(reverse);
+            if (reverse == i) {
+                System.out.println(reverse);
+            }
+        }
+
+        scan.close();
+    }
+}
