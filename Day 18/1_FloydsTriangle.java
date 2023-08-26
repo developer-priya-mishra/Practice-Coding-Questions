5baab47fa9ed74fe89f1178e246d3380140a08c9
developer-priya-mishra <mishrapriya549@gmail.com>
2023-08-26 20:35:05 +0530

    Day 18

---
+++ Day 18/1_FloydsTriangle.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class FloydsTriangle {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int n = scan.nextInt();
+
+        int num = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println(" ");
+        }
+
+        scan.close();
+    }
+}
