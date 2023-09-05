ad2386ecae769eb72b80a060f368f543759c2c25
developer-priya-mishra <mishrapriya549@gmail.com>
2023-09-05 21:51:09 +0530

    Day 28

---
+++ Day 28/12_DisariumNumber.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class DisariumNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int num = scan.nextInt();
+
+        int tempNum = num;
+        int count = 0;
+        while (tempNum != 0) {
+            count += 1;
+            tempNum = tempNum / 10;
+        }
+
+        tempNum = num;
+        int sum = 0;
+        while (tempNum != 0) {
+            for (int i = count; i >= 1; i--) {
+                int remainder = tempNum % 10;
+                double result = Math.pow(remainder, i);
+                sum += result;
+                tempNum = tempNum / 10;
+            }
+
+        }
+
+        if (sum == num) {
+            System.out.println(num + " is a Disarium Number");
+        } else {
+            System.out.println(num + " is not a Disarium Number");
+        }
+
+        scan.close();
+    }
+}
