b0dda141e31f939f027365a4ae7e36ee78a905ad
developer-priya-mishra <mishrapriya549@gmail.com>
2023-09-05 21:50:56 +0530

    Day 28

---
+++ Day 28/10_PerfectNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+// Perfect number is equal to the sum of its positive divisors, excluding the number itself. For instance, 6 has divisors 1, 2 and 3, and 1 + 2 + 3 = 6
+
+class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int num = scan.nextInt();
+
+        int sum = 0;
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+
+        if (sum == num) {
+            System.out.println(num + " is a Perfect Number");
+        } else {
+            System.out.println(num + " is not a Perfect Number");
+        }
+
+        scan.close();
+    }
+}
