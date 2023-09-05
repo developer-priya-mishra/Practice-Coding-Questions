843c315c8d54624ddc6de3f6dcc24498d8931dae
developer-priya-mishra <mishrapriya549@gmail.com>
2023-09-05 14:40:30 +0530

    Day 28

---
+++ Day 28/3_TwinPrimeNumber.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class TwinPrimeNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the first number : ");
+        int num1 = scan.nextInt();
+
+        System.out.print("Enter the second number : ");
+        int num2 = scan.nextInt();
+
+        if (checkPrimeNumber(num1) && checkPrimeNumber(num2)) {
+            if (num1 - num2 == 2 || num2 - num1 == 2) {
+                System.out.println(num1 + " and " + num2 + " is a twin prime number");
+            } else {
+                System.out.println(num1 + " and " + num2 + " is not a twin prime number");
+            }
+        } else {
+            System.out.println("Number is not a prime number");
+        }
+
+        scan.close();
+    }
+
+    static boolean checkPrimeNumber(int number) {
+        boolean isPrimeNumber = true;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                isPrimeNumber = false;
+                break;
+            }
+        }
+        return isPrimeNumber;
+    }
+}
