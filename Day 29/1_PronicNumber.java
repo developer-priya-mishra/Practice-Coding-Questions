dce10fcb783f8147bdd968eb69d6f63c4ec20238
developer-priya-mishra <mishrapriya549@gmail.com>
2023-09-07 21:28:17 +0530

    Added one more method

--- Day 29/1_PronicNumber.java
+++ Day 29/1_PronicNumber.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
 // Pronic number is a number which is the product of two consecutive integers. Eg, n = x * (x+1).
-class PronicNumber {
+// sqrt(n)*(sqrt(n)+1)=n
+
+class PronicNumber1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
@@ -9,15 +11,32 @@ class PronicNumber {
         int num = scan.nextInt();
 
         boolean isPronicNumber = false;
-        for (int i = 1; i <= num; i++) {
-            if (num % i == 0) {
-                if (i * (i + 1) == num) {
-                    isPronicNumber = true;
-                    break;
-                }
+        for (int i = 0; i <= num; i++) {
+            if (i * (i + 1) == num) {
+                isPronicNumber = true;
+                break;
             }
         }
         if (isPronicNumber) {
+            System.out.println(num + " is a Pronic Number");
+        } else {
+            System.out.println(num + " is not a Pronic Number");
+        }
+
+        scan.close();
+    }
+}
+
+class PronicNumber2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int num = scan.nextInt();
+
+        int squareRoot = (int) Math.sqrt(num);
+
+        if (squareRoot * (squareRoot + 1) == num) {
             System.out.println(num + " is a Pronic Number");
         } else {
             System.out.println(num + " is not a Pronic Number");
