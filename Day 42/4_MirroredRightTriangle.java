c17da5b529f133777e5c90abf232e2f20de8c3af
developer-priya-mishra <mishrapriya549@gmail.com>
2023-09-19 18:56:32 +0530

    Create 4_MirroredRightTriangle.java

---
+++ Day 42/4_MirroredRightTriangle.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class MirroredRightTriangle {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int num = scan.nextInt();
+
+        for (int row = num; row >= 1; row--) {
+            for (int col = 1; col <= num; col++) {
+                if (col >= row) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        scan.close();
+    }
+}
