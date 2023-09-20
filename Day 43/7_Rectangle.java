31f5563cf60ebbb930589a591ead4def65a58b9f
developer-priya-mishra <mishrapriya549@gmail.com>
2023-09-20 21:02:02 +0530

    Day 43

---
+++ Day 43/7_Rectangle.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Rectangle {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows : ");
+        int numRow = scan.nextInt();
+
+        System.out.print("Enter the number of columns : ");
+        int numCol = scan.nextInt();
+
+        if (numRow <= 1 || numCol <= 1) {
+            System.out.println("Number should be greater than 2");
+        } else if (numCol > numRow) {
+            for (int row = 1; row <= numRow; row++) {
+                for (int col = 1; col <= numCol; col++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("Number of columns should be greater than number of rows.");
+        }
+
+        scan.close();
+    }
+}
