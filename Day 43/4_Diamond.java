c1124d5196f31dffd741949543beb646b31cf4c9
developer-priya-mishra <mishrapriya549@gmail.com>
2023-09-20 21:01:41 +0530

    Day 43

---
+++ Day 43/4_Diamond.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class Diamond {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int num = scan.nextInt();
+
+        for (int row = num; row >= 1; row--) {
+            for (int col = 1; col <= num; col++) {
+                if (col >= row) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        for (int row = 1; row <= num; row++) {
+            for (int col = 1; col <= num; col++) {
+                if (col > row) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        scan.close();
+    }
+}
