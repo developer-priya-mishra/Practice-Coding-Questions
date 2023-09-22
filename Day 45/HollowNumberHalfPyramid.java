0707658c16a64a6f0e62390985dfc96f535a4338
developer-priya-mishra <mishrapriya549@gmail.com>
2023-09-22 23:18:13 +0530

    Day 45

---
+++ Day 45/HollowNumberHalfPyramid.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class HollowNumberHalfPyramid {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int num = scan.nextInt();
+
+        int j = 1;
+        for (int row = num; row >= 1; row--) {
+            for (int col = 1; col <= row; col++) {
+                if (row == num) {
+                    System.out.print(col + " ");
+                } else if (row == col) {
+                    System.out.print(num + " ");
+                } else if (col == 1) {
+                    System.out.print(j + " ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            j = j + 1;
+            System.out.println();
+        }
+
+        scan.close();
+    }
+}
