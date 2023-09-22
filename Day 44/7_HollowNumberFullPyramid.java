02fc8c4ea0992c5ce43f1ed396eddfc8b718c125
developer-priya-mishra <mishrapriya549@gmail.com>
2023-09-22 22:00:45 +0530

    Some Changes

--- Day 44/7_HollowNumberFullPyramid.java
+++ Day 44/7_HollowNumberFullPyramid.java
@@ -1,25 +1,54 @@
 import java.util.Scanner;
 
-class HollowNumberFullPyramid {
+class HollowNumberFullPyramid1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Enter the number : ");
         int num = scan.nextInt();
 
-        int j = 1;
         for (int row = num; row >= 1; row--) {
+            int j = 0;
             for (int col = 1; col <= num; col++) {
-                if (col >= row) {
+                if (col == row || col == num || row == 1) {
+                    j = j + 1;
                     System.out.print(j + " ");
                 } else {
-                    System.out.print(" ");
+                    if (col > row) {
+                        j = j + 1;
+                        System.out.print("  ");
+                    } else {
+                        System.out.print(" ");
+                    }
                 }
             }
             System.out.println();
-
         }
+        scan.close();
+    }
+}
 
+class HollowNumberFullPyramid2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int num = scan.nextInt();
+
+        for (int row = num; row >= 1; row--) {
+            int j = 1;
+            for (int col = 1; col <= num; col++) {
+                if (col == row) {
+                    System.out.print(j + "_");
+                } else if (col > row) {
+                    System.out.print(j + 1 + "_");
+                    j = j + 1;
+                } else {
+                    System.out.print("_");
+                }
+            }
+            System.out.println();
+        }
         scan.close();
     }
 }
