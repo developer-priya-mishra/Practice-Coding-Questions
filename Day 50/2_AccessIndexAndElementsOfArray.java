b8efa25d5534200e612390a786921b5502fc874b
developer-priya-mishra <mishrapriya549@gmail.com>
2023-09-27 18:06:54 +0530

    Day 50

---
+++ Day 50/2_AccessIndexAndElementsOfArray.java
@@ -0,0 +1,9 @@
+class AccessIndexAndElementsOfArray {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5 };
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Value of " + i + "th index is " + arr[i]);
+        }
+    }
+}
