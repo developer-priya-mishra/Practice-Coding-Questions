30cb803ab426f41eac2f96d0686d525eb4793057
developer-priya-mishra <mishrapriya549@gmail.com>
2023-10-03 16:09:30 +0530

    Day 55

---
+++ Day 55/9_SumOfArray.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class SumOfArray {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int maxSize = 10;
+        int arr[] = new int[maxSize];
+
+        System.out.print("Enter the size : ");
+        int size = scan.nextInt();
+
+        System.out.print("Enter the elements : ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = scan.nextInt();
+        }
+
+        System.out.print("Array are : ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + ", ");
+        }
+
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            sum += arr[i];
+        }
+        System.out.print("\nSum of array is : " + sum);
+
+        scan.close();
+
+    }
+}
