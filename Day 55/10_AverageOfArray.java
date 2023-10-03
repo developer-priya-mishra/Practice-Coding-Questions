13fa186e2fe11684054bdcc6b94bd9fc700c3241
developer-priya-mishra <mishrapriya549@gmail.com>
2023-10-03 16:09:35 +0530

    Day 55

---
+++ Day 55/10_AverageOfArray.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class AverageOfArray {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int maxSize = 10;
+        int arr[] = new int[maxSize];
+
+        System.out.print("Enter the size : ");
+        int size = scan.nextInt();
+
+        System.out.print("Enter the elements : ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = scan.nextInt();
+        }
+
+        System.out.print("Array are : ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + ", ");
+        }
+
+        int sum = 0;
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            sum += arr[i];
+            count = count + 1;
+        }
+        int averageOfNumber = sum / count;
+        System.out.print("\nAverage of array is : " + averageOfNumber);
+
+        scan.close();
+
+    }
+}
