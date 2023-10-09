7ea899c711f199abc690d07b71cceed91502c42d
developer-priya-mishra <mishrapriya549@gmail.com>
2023-10-09 21:05:57 +0530

    Create 1_GoldbachNumber.java

---
+++ Day 61/1_GoldbachNumber.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class GoldbachNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int num = scan.nextInt();
+
+        int arr[] = new int[num];
+        int index = 0;
+
+        for (int i = 3; i <= num; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                arr[index] = i;
+                index = index + 1;
+            }
+        }
+
+        int size = arr.length;
+        for (int j = 0; j < size; j++) {
+            System.out.print(arr[j] + ", ");
+        }
+
+        loop: for (int i = 0; i < size; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] + arr[j] == num) {
+                    System.out.print("\n" + num + " is a GoldbachNumber");
+                    break loop;
+                }
+            }
+        }
+
+        scan.close();
+    }
+}
