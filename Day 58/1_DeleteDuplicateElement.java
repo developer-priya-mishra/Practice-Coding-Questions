8a1d8fecc2313da7cfed5133206b2cf6b4d50857
developer-priya-mishra <mishrapriya549@gmail.com>
2023-10-13 20:20:14 +0530

    Update 1_DeleteDuplicateElement.java

--- Day 58/1_DeleteDuplicateElement.java
+++ Day 58/1_DeleteDuplicateElement.java
@@ -3,7 +3,7 @@ class DeleteDuplicateElement {
         int arr[] = { 1, 2, 1, 3, 3, 4, 1, 7, 7, 8, 8, 9, 9, 10, 10, 15 };
         int size = arr.length;
 
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size - 1; i++) {
             for (int j = i + 1; j < size; j++) {
                 if (arr[i] == arr[j]) {
                     arr[j] = arr[size - 1];
