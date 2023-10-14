1f3b22239a75bf34cf89a8fb60c49e8e87f30e1f
developer-priya-mishra <mishrapriya549@gmail.com>
2023-10-14 22:02:13 +0530

    Create 4_OctalToDecimal.java

---
+++ Day 66/4_OctalToDecimal.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class OctalToDecimal {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the Octal number : ");
+        int num = scan.nextInt();
+
+        int decimalNumber = 0;
+        int power = 0;
+        loop: while (num != 0) {
+            int remainder = num % 10;
+            if (remainder == 8 || remainder == 9) {
+                System.out.println("Invalid octal number");
+                break loop;
+            } else {
+                decimalNumber += remainder * Math.pow(8, power);
+                power += 1;
+                num = num / 10;
+            }
+        }
+        System.out.print("Decimal number is " + decimalNumber);
+
+        scan.close();
+    }
+}
