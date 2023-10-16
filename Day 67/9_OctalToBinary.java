d20d15b9986467210d4e847dc591a9bd5f922d38
developer-priya-mishra <mishrapriya549@gmail.com>
2023-10-16 16:12:54 +0530

    Update 9_OctalToBinary.java

--- Day 67/9_OctalToBinary.java
+++ Day 67/9_OctalToBinary.java
@@ -1,63 +1,114 @@
 import java.util.Scanner;
 
-class OctalToBinary {
+class OctalToBinary1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter the number : ");
-        int num = scan.nextInt();
+        int octalNumber = scan.nextInt();
 
-        int reverseBinary = 0;
+        int reverseBinary[] = new int[100];
+        int index = 0;
+
+        while (octalNumber != 0) {
+            int remainder = octalNumber % 10;
+            switch (remainder) {
+                case 0:
+                    remainder = 0;
+                    break;
+                case 1:
+                    remainder = 1;
+                    break;
+                case 2:
+                    remainder = 10;
+                    break;
+                case 3:
+                    remainder = 11;
+                    break;
+                case 4:
+                    remainder = 100;
+                    break;
+                case 5:
+                    remainder = 101;
+                    break;
+                case 6:
+                    remainder = 110;
+                    break;
+                case 7:
+                    remainder = 111;
+                    break;
+                default:
+                    break;
+            }
+            reverseBinary[index] = remainder;
+            octalNumber = octalNumber / 10;
+            index += 1;
+        }
+
         int binaryNumber = 0;
+        for (int i = index - 1; i >= 0; i--) {
+            binaryNumber = binaryNumber * 1000 + reverseBinary[i];
+        }
+        System.out.println("Binary Number is " + binaryNumber);
 
-        loop: while (num != 0) {
-            int remainder = num % 10;
-            if (remainder == 8 || remainder == 9) {
-                System.out.println(num + " is invalid octal number");
-                binaryNumber = -1;
-                break loop;
-            } else {
-                switch (remainder) {
-                    case 0:
-                        remainder = 000;
-                        break;
-                    case 1:
-                        remainder = 001;
-                        break;
-                    case 2:
-                        remainder = 010;
-                        break;
-                    case 3:
-                        remainder = 011;
-                        break;
-                    case 4:
-                        remainder = 100;
-                        break;
-                    case 5:
-                        remainder = 101;
-                        break;
-                    case 6:
-                        remainder = 110;
-                        break;
-                    case 7:
-                        remainder = 111;
-                        break;
-                    default:
-                        break;
-                }
-                reverseBinary = reverseBinary * 1000 + remainder;
-                num = num / 10;
+        scan.close();
+    }
+}
+
+class OctalToBinary2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int octalNumber = scan.nextInt();
+
+        StringBuilder binaryNumber = new StringBuilder();
+        while (octalNumber != 0) {
+            String remainder = Integer.toString(octalNumber % 10);
+            switch (remainder) {
+                case "0":
+                    remainder = "000";
+                    break;
+                case "1":
+                    remainder = "001";
+                    break;
+                case "2":
+                    remainder = "010";
+                    break;
+                case "3":
+                    remainder = "011";
+                    break;
+                case "4":
+                    remainder = "100";
+                    break;
+                case "5":
+                    remainder = "101";
+                    break;
+                case "6":
+                    remainder = "110";
+                    break;
+                case "7":
+                    remainder = "111";
+                    break;
+                default:
+                    break;
             }
+            binaryNumber.insert(0, remainder);
+            octalNumber = octalNumber / 10;
         }
 
-        if (binaryNumber != -1) {
-            while (reverseBinary != 0) {
-                int remainder = reverseBinary % 1000;
-                binaryNumber = binaryNumber * 1000 + remainder;
-                reverseBinary = reverseBinary / 1000;
-            }
+        System.out.println("Binary Number is " + binaryNumber);
+        scan.close();
+    }
+}
 
-            System.out.println("Binary Number is " + binaryNumber);
-        }
+class OctalToBinary3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        String octalNumber = scan.next();
+
+        int decimalNumber = Integer.parseInt(octalNumber, 8);
+        String binaryNumber = Integer.toBinaryString(decimalNumber);
+        System.out.println("Binary Number is " + binaryNumber);
         scan.close();
     }
 }
