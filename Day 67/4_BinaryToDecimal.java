3511420374feb336ef010e65bf9d6ab0e32bef22
developer-priya-mishra <mishrapriya549@gmail.com>
2023-10-16 16:12:06 +0530

    Update 4_BinaryToDecimal.java

--- Day 67/4_BinaryToDecimal.java
+++ Day 67/4_BinaryToDecimal.java
@@ -1,20 +1,34 @@
 import java.util.Scanner;
 
-class BinaryToDecimal {
+class BinaryToDecimal1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Enter the binary number : ");
-        int num = scan.nextInt();
+        int binaryNumber = scan.nextInt();
 
         double power = 0;
         int decimalNumber = 0;
-        while (num != 0) {
-            int remainder = num % 10;
+        while (binaryNumber != 0) {
+            int remainder = binaryNumber % 10;
             decimalNumber += remainder * Math.pow(2, power);
             power += 1;
-            num = num / 10;
+            binaryNumber = binaryNumber / 10;
         }
+
+        System.out.print("Decimal number is " + decimalNumber);
+        scan.close();
+    }
+}
+
+class BinaryToDecimal2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the binary number : ");
+        String binaryNumber = scan.next();
+
+        int decimalNumber = Integer.parseInt(binaryNumber, 2);
 
         System.out.print("Decimal number is " + decimalNumber);
         scan.close();
