f9226b496e81263165fe150daa3410bce1b619ef
developer-priya-mishra <mishrapriya549@gmail.com>
2023-10-16 16:11:59 +0530

    Update 2_DecimalToOctal.java

--- Day 67/2_DecimalToOctal.java
+++ Day 67/2_DecimalToOctal.java
@@ -1,18 +1,59 @@
 import java.util.Scanner;
 
-class DecimalToOctal {
+class DecimalToOctal1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Enter the decimal number : ");
-        int num = scan.nextInt();
+        int decimalNumber = scan.nextInt();
+
+        int reverseOctalNumber[] = new int[100];
+        int index = 0;
+        while (decimalNumber != 0) {
+            int remainder = decimalNumber % 8;
+            reverseOctalNumber[index] = remainder;
+            decimalNumber = decimalNumber / 8;
+            index += 1;
+        }
+
+        int octalNumber = 0;
+        for (int i = index - 1; i >= 0; i--) {
+            octalNumber = octalNumber * 10 + reverseOctalNumber[i];
+        }
+        System.out.print("Octal number is " + octalNumber);
+
+        scan.close();
+    }
+}
+
+class DecimalToOctal2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the decimal number : ");
+        int decimalNumber = scan.nextInt();
 
         StringBuilder octalNumber = new StringBuilder();
-        while (num != 0) {
-            int remainder = num % 8;
+        while (decimalNumber != 0) {
+            int remainder = decimalNumber % 8;
             octalNumber.insert(0, remainder);
-            num = num / 8;
+            decimalNumber = decimalNumber / 8;
         }
+        System.out.print("Octal number is " + octalNumber);
+
+        scan.close();
+    }
+}
+
+class DecimalToOctal3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the decimal number : ");
+        int decimalNumber = scan.nextInt();
+
+        String octalNumber = Integer.toOctalString(decimalNumber);
+
         System.out.print("Octal number is " + octalNumber);
 
         scan.close();
