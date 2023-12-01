17856489a9af8fd5ee6bd31132dd92d6d5a548ea
developer-priya-mishra <mishrapriya549@gmail.com>
2023-12-01 15:51:59 +0530

    Create 4_PrimeNumber.java

---
+++ Day 114 - Revision/4_PrimeNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = scan.nextInt();
+
+        boolean isPrime = false;
+        for (int i = 2; i < num; i++) {
+
+        }
+    }
+}
