d7631e319db6ce3b7787dfe2bfa2a0bbde9ae438
developer-priya-mishra <mishrapriya549@gmail.com>
2023-12-05 21:45:15 +0530

    Create 6_HappyNumber.java

---
+++ Day 115 - Revision/6_HappyNumber.java
@@ -0,0 +1,7 @@
+class HappyNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = scan.nextInt();
+    }
+}
