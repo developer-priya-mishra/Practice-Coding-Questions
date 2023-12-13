11b57d1362b390701b47426a9d53e3cbca677f85
developer-priya-mishra <mishrapriya549@gmail.com>
2023-12-13 23:41:40 +0530

    Create 5_AccessIndexOfArray.java

---
+++ Day 126 (Revision)/5_AccessIndexOfArray.java
@@ -0,0 +1,9 @@
+class AccessIndexOfArray {
+    public static void main(String[] args) {
+        int arr[] = { 1, 4, 6, 20, 78, 0, 365 };
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + ", ");
+        }
+    }
+}
