089c3616218274efbc2ae582e721d7d607f82b58
developer-priya-mishra <mishrapriya549@gmail.com>
2023-12-13 23:41:44 +0530

    Create 6_LastElementOfList.java

---
+++ Day 126 (Revision)/6_LastElementOfList.java
@@ -0,0 +1,8 @@
+class LastElementOfList {
+    public static void main(String[] args) {
+        int arr[] = { 1, 4, 6, 20, 78, 0, 365 };
+
+        int lastIndex = arr.length;
+        System.out.print("Last element of array is " + arr[lastIndex - 1]);
+    }
+}
