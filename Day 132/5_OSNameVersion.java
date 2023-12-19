4fc6770f8d7abb71314627a0ae6f93b83046f60a
developer-priya-mishra <mishrapriya549@gmail.com>
2023-12-19 18:57:11 +0530

    Create 5_OSNameVersion.java

---
+++ Day 132/5_OSNameVersion.java
@@ -0,0 +1,6 @@
+class OSNameVersion {
+    public static void main(String[] args) {
+        System.out.println("OS Name : " + System.getProperty("os.name"));
+        System.out.println("OS Version : " + System.getProperty("os.version"));
+    }
+}
