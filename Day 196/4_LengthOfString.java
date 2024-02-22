c6e26a82d529dc343bc13c4cc21714047cb98869
developer-priya-mishra <mishrapriya549@gmail.com>
2024-02-22 09:24:56 +0530

    Create 4_LengthOfString.java

---
+++ Day 196/4_LengthOfString.java
@@ -0,0 +1,10 @@
+class LengthOfString {
+    public static void main(String[] args) {
+        String s = "Priya";
+        System.out.println(s.length());
+
+        int arr[] = { 1, 2, 4, 5, 6, 7 };
+        System.out.println(arr.length);
+
+    }
+}
