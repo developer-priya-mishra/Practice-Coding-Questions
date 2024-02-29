f89c62eaa51f9627eec6be8ac765c32df8f476d7
developer-priya-mishra <mishrapriya549@gmail.com>
2024-02-29 16:32:35 +0530

    Create 11_StringStart&EndWithCharacter.java

---
+++ Day 197/11_StringStart&EndWithCharacter.java
@@ -0,0 +1,17 @@
+class StringStartEndWithCertainCharacter {
+    public static void main(String[] args) {
+        String str = "Priya";
+
+        if (str.startsWith("p") == true) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+
+        if (str.endsWith("a") == true) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
