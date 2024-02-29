75b2a913901eef597152ac0113b6a9fef29dbf77
developer-priya-mishra <mishrapriya549@gmail.com>
2024-02-29 16:32:23 +0530

    Create 1_ConcatenateTwoString.java

---
+++ Day 197/1_ConcatenateTwoString.java
@@ -0,0 +1,9 @@
+class ConcatenateTwoString {
+    public static void main(String[] args) {
+        String s1 = "Priya";
+        String s2 = "Mishra";
+        System.err.println(s1 + " " + s2);
+
+        System.out.println(s1.concat("Aryansh"));
+    }
+}
