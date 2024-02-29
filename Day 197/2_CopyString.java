84eb3bae7d06ba255bbb608a208941ddf35ae445
developer-priya-mishra <mishrapriya549@gmail.com>
2024-02-29 16:31:54 +0530

    Create 2_CopyString.java

---
+++ Day 197/2_CopyString.java
@@ -0,0 +1,11 @@
+class CopyString {
+    public static void main(String[] args) {
+        String s1 = "Priya";
+        String s2 = s1;
+        System.err.println(s1);
+        System.out.println(s2);
+        s1 = "Aryansh";
+        System.err.println(s1);
+        System.out.println(s2);
+    }
+}
