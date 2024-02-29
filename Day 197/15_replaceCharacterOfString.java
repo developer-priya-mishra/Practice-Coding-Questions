169f5c58b968125dfcb7dcaae94359e6a539274f
developer-priya-mishra <mishrapriya549@gmail.com>
2024-02-29 16:32:17 +0530

    Create 15_replaceCharacterOfString.java

---
+++ Day 197/15_replaceCharacterOfString.java
@@ -0,0 +1,9 @@
+class replaceCharacterOfString {
+    public static void main(String[] args) {
+        String str1 = "Priym";
+        System.out.println(str1);
+
+        str1 = str1.replace('m', 'a');
+        System.out.println(str1);
+    }
+}
