efb2e51dd2ccfcb6113b11193474b7cc67ffd06d
developer-priya-mishra <mishrapriya549@gmail.com>
2024-02-29 16:32:14 +0530

    Create 16_ReplaceAllOccurenceOfSpecifiedCharacterInString.java

---
+++ Day 197/16_ReplaceAllOccurenceOfSpecifiedCharacterInString.java
@@ -0,0 +1,7 @@
+class ReplaceAllOccurenceOfSpecifiedCharacterInString {
+    public static void main(String[] args) {
+        String str = "This is Spoons";
+        str = str.replaceAll("s", "es");
+        System.out.println(str);
+    }
+}
