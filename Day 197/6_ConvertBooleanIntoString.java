3288e36f343e381025306c6b808153ed702addb0
developer-priya-mishra <mishrapriya549@gmail.com>
2024-02-29 16:31:38 +0530

    Create 6_ConvertBooleanIntoString.java

---
+++ Day 197/6_ConvertBooleanIntoString.java
@@ -0,0 +1,7 @@
+class ConvertBooleanIntoString {
+    public static void main(String[] args) {
+        boolean bool = true;
+        String str = String.valueOf(bool);
+        System.out.println(str);
+    }
+}
