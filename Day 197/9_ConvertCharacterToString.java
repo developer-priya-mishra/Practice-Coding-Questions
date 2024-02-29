f1396313fee9a98f9b9d5b806cd275ca1e17221c
developer-priya-mishra <mishrapriya549@gmail.com>
2024-02-29 16:32:26 +0530

    Create 9_ConvertCharacterToString.java

---
+++ Day 197/9_ConvertCharacterToString.java
@@ -0,0 +1,7 @@
+class ConvertCharacterToString {
+    public static void main(String[] args) {
+        char c = 'P';
+        String str = String.valueOf(c);
+        System.out.println(str + "riya");
+    }
+}
