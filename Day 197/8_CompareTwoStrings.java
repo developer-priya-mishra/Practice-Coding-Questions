46d54e04823b3a8f305ebdfccf175ad9e557c60a
developer-priya-mishra <mishrapriya549@gmail.com>
2024-02-29 16:31:34 +0530

    Create 8_CompareTwoStrings.java

---
+++ Day 197/8_CompareTwoStrings.java
@@ -0,0 +1,12 @@
+class CompareTwoStrings {
+    public static void main(String[] args) {
+        String str1 = "Priya";
+        String str2 = "priya";
+
+        if (str1.equals(str2)) {
+            System.out.println("Both strings are equal");
+        } else {
+            System.out.println("Both strings are not equal");
+        }
+    }
+}
