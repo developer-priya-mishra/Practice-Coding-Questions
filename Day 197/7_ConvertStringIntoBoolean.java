96e73d7c958a7cb952b4a10238d599510d109045
developer-priya-mishra <mishrapriya549@gmail.com>
2024-02-29 16:31:41 +0530

    Create 7_ConvertStringIntoBoolean.java

---
+++ Day 197/7_ConvertStringIntoBoolean.java
@@ -0,0 +1,7 @@
+class ConvertStringIntoBoolean {
+    public static void main(String[] args) {
+        String str = "true";
+        boolean bool = Boolean.valueOf(str);
+        System.out.println(bool);
+    }
+}
